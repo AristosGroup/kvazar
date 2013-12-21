dcc7842abe9a38ce5a5a7b5a2af4c817faede8db
Aleksey Kuznetsov <mrakobesov@gmail.com>
2013-12-21 16:40:11 +0400

    issue detail template

--- client/helpers/gravatar.js
+++ client/helpers/gravatar.js
@@ -1,12 +1,12 @@
 // super, super simple
 Gravatar = {
     getGravatar: function(user, options) {
-        if(user.email_hash){
+        if(user.email()){
             var options = options || {};
 
             var protocol = options.secure ? 'https' : 'http';
             delete options.secure;
-            var hash = user.email_hash;
+            var hash = md5(user.email());
             var url = protocol + '://www.gravatar.com/avatar/' + hash;
 
             var params = _.map(options, function(val, key) { return key + "=" + val}).join('&');
@@ -15,8 +15,6 @@ Gravatar = {
 
             return url;
 
-        }else if(user.services && user.services.twitter){
-            return user.services.twitter.profile_image_url; //for the oauth-login avatar, diff for diff oauth, maybe it could be better
         }
     }
 };
